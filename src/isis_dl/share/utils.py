40eefb737e651a6ccb76f4658b934ddc15b02a16
Emily3403 <seebeckemily3403@gmail.com>
2021-10-24 16:15:32 +0200

    Added download limiter

--- src/isis_dl/share/utils.py
+++ src/isis_dl/share/utils.py
@@ -3,11 +3,8 @@ from __future__ import annotations
 
 import argparse
 import atexit
-import datetime
-import enum
 import inspect
 import logging
-import math
 import os
 import signal
 import sys
@@ -15,18 +12,14 @@ import time
 from dataclasses import dataclass
 from functools import wraps
 from queue import PriorityQueue
-from threading import Thread
-from typing import Union, Dict, Callable, Optional, cast, List, Tuple, Iterable, Any
+from typing import Union, Dict, Callable, Optional, List, Tuple
 from urllib.parse import unquote
 
-import requests
-from bs4 import BeautifulSoup
-
-import isis_dl.backend.api as api
-from isis_dl.share.settings import working_dir_location, checksum_algorithm, sleep_time_for_isis, download_chunk_size, progress_bar_resolution, ratio_to_skip_big_progress, \
-    whitelist_file_name_location, \
+from isis_dl.share.settings import working_dir_location, whitelist_file_name_location, \
     blacklist_file_name_location, log_file_location, is_windows, log_clear_screen, settings_file_location, download_dir_location, temp_dir_location, password_dir, intern_dir_location, \
     log_dir_location, course_name_to_id_file_location
+
+import isis_dl.share.settings as settings
 
 
 def get_args():
@@ -41,8 +34,9 @@ def get_args():
 
     parser.add_argument("-V", "--version", help="Print the version number and exit", action="store_true")
     parser.add_argument("-v", "--verbose", help="Enable debug output", action="store_true")
-    parser.add_argument("-n", "--num-threads", help="The number of threads which download the content from an individual course. (This is multiplied by the number of courses)", type=check_positive,
-                        default=8)
+    parser.add_argument("-n", "--num-threads", help="The number of threads which download the content from an individual course.", type=check_positive,
+                        default=4)
+    parser.add_argument("-d", "--download-rate", help="Limits the download rate to {…}MiB/s", type=float, default=20)
 
     parser.add_argument("-o", "--overwrite", help="Overwrites all existing files i.e. re-downloads them all.", action="store_true")
     parser.add_argument("-f", "--file-list", help="The the downloaded files in a summary at the end.\nThis is meant as a debug feature.", action="store_true")
@@ -116,7 +110,7 @@ def startup():
 
     prepare_file(course_name_to_id_file_location)
 
-    create_link_to_settings_file(os.path.abspath(__file__))
+    create_link_to_settings_file(os.path.abspath(settings.__file__))
     prepare_file(whitelist_file_name_location)
     prepare_file(blacklist_file_name_location)
 
@@ -220,18 +214,6 @@ def debug_time(str_to_put: Optional[str] = None, func_to_call: Optional[Callable
     return decorator
 
 
-class MySession(requests.Session):
-    def __init__(self, key: str):
-        super().__init__()
-        self.key = key
-
-    def __str__(self):
-        return f"Session with {self.key}"
-
-    def __repr__(self):
-        return self.__str__()
-
-
 class OnKill:
     _funcs: PriorityQueue[Tuple[int, Callable[[], None]]] = PriorityQueue()
     _min_priority = 0
@@ -292,79 +274,6 @@ def on_kill(priority: Optional[int] = None):
 OnKill()
 
 
-class MediaType(enum.Enum):
-    video = enum.auto()
-    archive = enum.auto()
-    document = enum.auto()
-
-    @property
-    def dir_name(self):
-        if self == MediaType.video:
-            return "Videos/"
-        elif self == MediaType.archive:
-            return "Archives/"
-        else:
-            return "Material/"
-
-    @staticmethod
-    def list_dirs() -> Iterable[str]:
-        return "Videos/", "Material/", "Archives/"
-
-    @staticmethod
-    def list_excluded_dirs() -> Iterable[str]:
-        return "UnpackedArchives",
-
-
-class DownloadStatus(enum.Enum):
-    not_started = enum.auto()
-    started = enum.auto()
-    succeeded = enum.auto()
-
-    found_by_checksum = enum.auto()
-    suspended = enum.auto()
-    failed = enum.auto()
-
-    stopped_done = enum.auto()
-    force_stopped = enum.auto()
-
-    @property
-    def done(self):
-        # Canonically done → not forced
-        return self in {DownloadStatus.succeeded, DownloadStatus.found_by_checksum, DownloadStatus.failed}
-
-    @property
-    def done_or_stopped(self):
-        return self in {DownloadStatus.succeeded, DownloadStatus.found_by_checksum, DownloadStatus.failed, DownloadStatus.force_stopped, DownloadStatus.stopped_done}  # O(1) lookup time goes brr
-
-    @property
-    def downloading(self):
-        # Will eventually download
-        return self in {DownloadStatus.not_started, DownloadStatus.started, DownloadStatus.suspended}
-
-    @staticmethod
-    def make_progress_bar(item: MediaContainer):
-        start, end = "╶", "╴"
-
-        def chop_to_last_10(num: Optional[float]) -> Optional[int]:
-            if num is None:
-                return None
-
-            return int(num * progress_bar_resolution)
-
-        progress = chop_to_last_10(item.percent_done)
-
-        string = start
-        if progress is None:
-            string += "~" * progress_bar_resolution
-
-        else:
-            string += "█" * progress + " " * (progress_bar_resolution - progress)
-
-        string += end
-
-        return string
-
-
 # Shared between modules.
 @dataclass
 class User:
@@ -381,6 +290,7 @@ class User:
         return self.username + "\n" + self.password + "\n"
 
 
+# TODO: Probably delete
 @dataclass
 class Video:
     name: str
@@ -412,104 +322,6 @@ class Video:
                 return False
             return self.name > other.name
         return other.created > self.created
-
-
-class Status(Thread):
-    _instance = None
-    _running = True
-
-    def __new__(cls):
-        # Singleton
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self, files: Optional[List[MediaContainer]] = None):
-        self.files = files or []
-        super().__init__()
-        self.daemon = True
-        self.start()
-
-    def add_files(self, files: List[MediaContainer]):
-        self.files.extend(files)
-
-    def run(self) -> None:
-        while Status._running:
-            time.sleep(args.status_time)
-            if not self.files:
-                continue
-            clear_screen()
-            # Gather information
-            skipped, failed, exited, currently_downloading, finished = [], [], [], [], []
-
-            for item in self.files:
-                if item.status.done_or_stopped:
-                    finished.append(item)
-
-                if item.status == DownloadStatus.found_by_checksum:
-                    skipped.append(item)
-
-                if item.status == DownloadStatus.stopped_done:
-                    exited.append(item)
-
-                if item.status == DownloadStatus.failed:
-                    failed.append(item)
-
-                if item.status == DownloadStatus.started:
-                    currently_downloading.append(item)
-
-            def format_int(num: int):
-                # log_10(num) = number of numbers
-                return f"{num:{' '}>{math.ceil(math.log10(len(self.files) or 1))}}"
-
-            def format_lst(lst: List[Any]):
-                return format_int(len(lst)) + " " * (len(format_int(len(self.files))) + 3)
-
-            logger.info(
-                "\n -- Status --\n" +
-                f"Finished: {format_int(len(finished))} / {format_int(len(self.files))} files\n" +
-                f"Skipped:  {format_lst(skipped)} files (Checksum)\n" +
-                f"Skipped:  {format_lst(exited)} files (Exit)\n" +
-                f"Failed:   {format_lst(failed)} files"
-            )
-
-            # Now determine the the already downloaded amount and display it
-            if currently_downloading:
-                done: List[Tuple[Union[int, float], str]] = [HumanBytes.format(num.already_downloaded) for num in currently_downloading]  # type: ignore
-                first = e_format([num[0] for num in done])
-                first_units = [item[1] for item in done]
-
-                max_values: List[Tuple[Union[int, float], str]] = [HumanBytes.format(num.size) for num in currently_downloading]  # type: ignore
-                second = e_format([num[0] for num in max_values])
-                second_units = [item[1] if item[0] is not None else '   ' for item in max_values]
-
-                progress_str = [item.status.make_progress_bar(item) for item in currently_downloading]
-
-                final_str = f"Currently downloading: {len(currently_downloading)} files\n\n"
-
-                final_middle = [
-                    (container.percent_done or 0, f"{progress} [{already} {already_unit} / {size} {size_unit}] - {container.name}")
-
-                    for container, already, already_unit, size, size_unit, progress in
-                    zip(currently_downloading, first, first_units, second, second_units, progress_str)
-                ]
-
-                final_str += "\n".join(item[1] for item in sorted(final_middle, key=lambda x: x[0], reverse=True))
-
-                # Maybe pad to num_threads of rows
-                if len(skipped) / len(self.files) < ratio_to_skip_big_progress:
-                    final_str += "\n" * (args.num_threads - len(currently_downloading) + 1)
-
-                if exited:
-                    logger.info("Please wait for shutdown…")
-                    logger.info(final_str)
-                else:
-                    logger.debug(final_str)
-
-    @staticmethod
-    @on_kill(-1)
-    def finish():
-        Status._running = False
 
 
 # Copied and adapted from https://stackoverflow.com/a/63839503
@@ -573,179 +385,6 @@ def e_format(
     return final
 
 
-@dataclass
-class MediaContainer:
-    media_type: MediaType
-    parent_course: api.Course
-    s: requests.Session
-    url: str
-    name: str
-    date: Optional[datetime.datetime] = None
-    running_download: Optional[requests.Response] = None
-    hash: Optional[str] = None
-    status: DownloadStatus = DownloadStatus.not_started
-    already_downloaded: int = 0
-
-    def __post_init__(self):
-        self.name = sanitize_name_for_dir(self.name)
-
-        if self.date is None:
-            self.date = datetime.datetime.now()
-
-    @staticmethod
-    def name_from_url(name: str) -> str:
-        return name.split("/")[-1].split("?")[0]
-
-    @classmethod
-    def from_video(cls, s: requests.Session, video: Dict[str, str], parent_course):
-        timestamp = datetime.datetime.fromtimestamp(cast(int, video["timecreated"]))
-        return cls(MediaType.video, parent_course, s, video["url"], video["title"] + video["fileext"], date=timestamp)
-
-    @classmethod
-    def from_url(cls, s: MySession, url: str, parent_course, session_key: Optional[str] = None):
-        if "isis" not in url:
-            # Don't go downloading other stuff.
-            return
-
-        elif any(item in url for item in ["mod/url", "mod/page", "mod/forum"]):
-            # These links dont lead to actual files. Ignore them
-            return
-
-        filename, media_type, running_dl = None, MediaType.document, None
-
-        if "mod/folder" in url:
-            folder_id = url.split("id=")[-1]
-            # Use the POST form
-            running_dl = s.get("https://isis.tu-berlin.de/mod/folder/download_folder.php", params={"id": folder_id, "sesskey": session_key}, stream=True)
-            if not running_dl.ok:
-                logger.warning(f"The folder {folder_id} from {url = } (Course: {parent_course}) could not be downloaded.\nReason: {running_dl.reason}")
-                return
-
-            filename = running_dl.headers["content-disposition"].split("filename*=UTF-8\'\'")[-1].strip('"')
-            media_type = MediaType.archive
-
-        elif "mod/resource" in url:
-            # Follow the link and get the file
-            req = s.get(url, allow_redirects=False)
-
-            if req.status_code != 303:
-                # TODO: Still download the content
-                logger.warning(f"The {url = } (Course = {parent_course}) does not redirect.  I am going to ignore it!")
-                return
-
-            redirect = BeautifulSoup(req.text)
-
-            links = redirect.find_all("a")
-            if len(links) > 1:
-                logger.warning(f"I've found {len(links) = } (Course = {parent_course}) many links. This should be debugged!")
-
-            url = links[0].attrs["href"]
-            filename = MediaContainer.name_from_url(url)
-
-        if filename is None:
-            logger.warning(f"The filename is None. (Course = {parent_course}) This is probably a bug. Please investigate!\n{url = }")
-            filename = "Did not find filename - " + checksum_algorithm(os.urandom(64)).hexdigest()
-
-        return cls(media_type, parent_course, s, url, filename, running_download=running_dl)
-
-    def download(self) -> None:
-        if self.status == DownloadStatus.stopped_done:
-            return
-
-        if not self.status.not_started:
-            logger.warning(f"You have prompted a download of a already started / finished file. This could be a bug! {self.status = }")
-            return
-
-        self.status = DownloadStatus.suspended
-        if self.running_download is None:
-            while True:
-                try:
-                    self.running_download = self.s.get(self.url, stream=True)
-                    break
-                except requests.exceptions.ConnectionError:
-                    logger.warning(f"ISIS is complaining about the number of downloads (I am ignoring it). Maybe consider dropping the thread count. Sleeping for {sleep_time_for_isis}s.")
-                    time.sleep(sleep_time_for_isis)
-
-        self.status = DownloadStatus.started
-
-        if not self.running_download.ok:
-            logger.error(f"The running download ({self}) is not okay: Status: {self.running_download.status_code} - {self.running_download.reason} "
-                         f"(Course: {self.parent_course.name}). Aborting!")
-            self.status = DownloadStatus.failed
-            return
-
-        self.hash, chunk = self.parent_course.checksum_handler.maybe_get_chunk(self.running_download.raw, self.name)
-
-        if chunk is None:
-            self.status = DownloadStatus.found_by_checksum
-            return
-
-        with open(self.parent_course.path(self.media_type.dir_name, self.name), "wb") as f:
-            f.write(chunk)
-            remaining = self.size
-            if remaining is not None:
-                remaining -= len(chunk)
-
-            # TODO: Maybe performance is better if we read entire file?
-            while remaining is None or remaining > 0:
-                if self.status == DownloadStatus.force_stopped:
-                    self.status = DownloadStatus.stopped_done
-                    return
-
-                new = self.running_download.raw.read(download_chunk_size)
-                if len(new) == 0:
-                    # No file left
-                    break
-
-                self.already_downloaded += len(new)
-                if remaining is not None:
-                    remaining -= len(new)
-
-                f.write(new)
-
-        self.status = DownloadStatus.succeeded
-        self.running_download = None
-
-        # Only register the hash after successfully downloading the file
-        self.parent_course.checksum_handler.add(self.hash)
-
-    def stop_download(self, tried_previously: bool = False):
-        if self.status == DownloadStatus.not_started:
-            self.status = DownloadStatus.force_stopped if tried_previously else DownloadStatus.stopped_done
-
-    @property
-    def size(self) -> Optional[int]:
-        if self.running_download is None:
-            return None
-        try:
-            return int(self.running_download.headers["content-length"])
-        except KeyError:
-            return None
-
-    @property
-    def percent_done(self) -> Optional[float]:
-        if self.running_download is None or self.size is None:
-            return None
-        try:
-            return self.already_downloaded / self.size
-        except KeyError:
-            return None
-
-    def __lt__(self, other):
-        try:
-            self.percent_done < other.percent_done
-        except TypeError:
-            # If they aren't comparable by percent do it by name
-            return self.name < other.name
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return f"[{HumanBytes.format(self.already_downloaded)} / {HumanBytes.format(self.size)}] \t {self}"
-
-
 startup()
 args = get_args()
 logger = get_logger()
-status = Status()
