3d857a3f9bc3177b41f6302d8a72fe281ceb1ed3
Emily3403 <seebeckemily3403@gmail.com>
2021-11-02 22:19:00 +0100

    Fixed Malformed file bug

--- src/isis_dl/backend/crypt.py
+++ src/isis_dl/backend/crypt.py
@@ -73,7 +73,7 @@ def get_credentials() -> User:
 
             if len(lines) not in {0, 2}:
                 logger.error(f"Malformed file: {path(clear_password_file)}. Expected 2 lines, found {len(lines)}.")
-            else:
+            elif lines:
                 return User(*lines)
 
     # Now check encrypted file
