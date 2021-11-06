f55583dd34d1c3486363f83193190a296593655e
Emily3403 <seebeckemily3403@gmail.com>
2021-11-06 16:10:30 +0100

    Try again

--- tests/test_0_first_install.py
+++ tests/test_0_first_install.py
@@ -23,8 +23,4 @@ def test_working_dir_structrue():
 
 def test_settings_link():
     import isis_dl
-    stats0 = os.stat(isis_dl.share.settings.__file__)
-    stats1 = os.stat(path(settings_file_location))
-
-    assert stats0 == stats1
-    assert stats0.st_nlink == 2
+    assert isis_dl.share.settings.__file__ == os.readlink(path(settings_file_location))
