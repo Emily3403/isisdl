42473285b8951a0b1a5ab803fc7bea264ea436c9
Emily3403 <seebeckemily3403@gmail.com>
2022-03-16 01:40:51 +0100

    Modified: src/isisdl/server/wsgi.py

--- src/isisdl/server/wsgi.py
+++ src/isisdl/server/wsgi.py
@@ -4,7 +4,16 @@ from typing import List, Any
 
 def application(env: Any, start_response: Any) -> List[bytes]:
     print(env)
-    print(env["wsgi.file_wrapper"]())
+
+    try:
+        length = int(env.get('CONTENT_LENGTH', '0'))
+    except ValueError:
+        length = 0
+
+    body = env['wsgi.input'].read(length)
+
+    print(body)
+
 
     start_response('200 OK', [('Content-Type', 'text/html')])
     response = "Hello"
