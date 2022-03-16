feeef50de750df46a70c6d529515dc28aed530f9
Emily3403 <seebeckemily3403@gmail.com>
2022-03-16 01:55:28 +0100

    Modified: src/isisdl/server/wsgi.py

--- src/isisdl/server/wsgi.py
+++ src/isisdl/server/wsgi.py
@@ -21,13 +21,10 @@ def application(env: Any, start_response: Any) -> List[bytes]:
         today = datetime.now().strftime("%y-%m-%d")
         os.makedirs("/home/isisdl-server/isisdl/src/isisdl/server/logs/v1/" + today, exist_ok=True)
         with open("/home/isisdl-server/isisdl/src/isisdl/server/logs/v1/" + today + "/" + sha256(str(time.time()).encode()).hexdigest(), "w") as f:
-            f.write(json.dumps(dat, indent=4))
+            f.write(json.dumps(dat, indent=4))  # TODO: indent away
 
     except Exception as ex:
-        print(f"Did not work: {ex}")
         pass
-
-    print(body)
 
 
     start_response('200 OK', [('Content-Type', 'text/html')])
