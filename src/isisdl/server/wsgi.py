4221fdbc345c35714e37d0cf863696a2843aec67
Emily3403 <seebeckemily3403@gmail.com>
2022-03-16 01:45:16 +0100

    Modified: src/isisdl/server/wsgi.py

--- src/isisdl/server/wsgi.py
+++ src/isisdl/server/wsgi.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
+import json
+from datetime import datetime
 from typing import List, Any
 
+working_dir =
 
 def application(env: Any, start_response: Any) -> List[bytes]:
-    print(env)
-
     try:
         length = int(env.get('CONTENT_LENGTH', '0'))
     except ValueError:
         length = 0
 
     body = env['wsgi.input'].read(length)
+
+    try:
+        dat = json.loads(str(body))
+        with open("/home/isisdl-server/isisdl/src/isisdl/server/logs/v1/" + datetime.now().strftime("%y-%m-%d")) as f:
+            f.write(json.dumps(dat, indent=4))
+
+    except Exception:
+        dat = None
+
 
     print(body)
 
