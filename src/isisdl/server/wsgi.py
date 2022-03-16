63aafb2a2d96b29f32adddd912c69a58d00f66c7
Emily3403 <seebeckemily3403@gmail.com>
2022-03-16 01:55:59 +0100

    Modified: src/isisdl/server/wsgi.py

--- src/isisdl/server/wsgi.py
+++ src/isisdl/server/wsgi.py
@@ -15,17 +15,14 @@ def application(env: Any, start_response: Any) -> List[bytes]:
     body: bytes = env['wsgi.input'].read(length)
 
     try:
-        dat = json.loads(body.decode())
-
-        print("uhhh")
         today = datetime.now().strftime("%y-%m-%d")
         os.makedirs("/home/isisdl-server/isisdl/src/isisdl/server/logs/v1/" + today, exist_ok=True)
         with open("/home/isisdl-server/isisdl/src/isisdl/server/logs/v1/" + today + "/" + sha256(str(time.time()).encode()).hexdigest(), "w") as f:
-            f.write(json.dumps(dat, indent=4))  # TODO: indent away
+            # Validate that the data is json
+            f.write(json.dumps(json.loads(body.decode()), indent=4))  # TODO: indent away
 
     except Exception as ex:
         pass
 
 
-    start_response('200 OK', [('Content-Type', 'text/html')])
     return []
