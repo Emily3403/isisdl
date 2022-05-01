00f92853115b404b1a9cb682ad4ce43bfe29f153
Emily3403 <seebeckemily3403@gmail.com>
2022-05-01 15:01:36 +0200

    Modified: compile/compile_isisdl.bat

--- compile/compile_isisdl.bat
+++ compile/compile_isisdl.bat
@@ -1,6 +1,11 @@
-del venv/
-del *.build
-del *.dist
+del /q venv
+del /q *.build
+del /q *.dist
 
 py -m venv venv
 .\venv\Scripts\activate
+
+pip install -e ..
+pip install nuitka zstandard
+
+nuitka3 --standalone --onefile --linux-onefile-icon=python_icon.png ../src/isisdl/__main__.py
